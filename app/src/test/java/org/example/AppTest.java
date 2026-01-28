c7bf623ffa63eeb88db03041c799f7d75d61c53e
grubbles17 <46872313+grubbles17@users.noreply.github.com>
2026-01-28 12:51:52 -0600

    Add test for dependent deduction calculation
    Added a test for dependent deduction calculation in AppTest.

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -6,6 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
   // You can put your JUnit tests here
   // Feel free to create other files, as needed, to organize your tests
+  @Test
+  public void testDependentDeductionCalculation(Object PayrollCalculator){
+    assertEquals(35, PayrollCalculator.perDependant(1), 0.001);
+  }
 
   @Test
   void itWorks() {
